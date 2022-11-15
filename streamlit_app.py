c8a68e9b8110b83be556ec26be27e38e427cd85a
kuziModjoul <kuzi@modjoul.com>
2022-11-15 12:06:43 -0500

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,23 @@
 import streamlit as st
 
-st.markdown("This is a test streamlit app")
+
+
+st.set_page_config( page_title="Streamlit Test App",
+                    page_icon= "random",
+                    layout="wide"
+ )
+
+col1, col2, col3 = st.columns((.1,1,.1))
+
+with col1:
+    st.write("")
+
+with col2:
+    st.markdown(" <h1 style='text-align: center;'> This is an Example Streamlit with Authentication</h1>", unsafe_allow_html=True)
+
+with col3:
+    st.write("")
+
+## We have  to authenticate the user before they can view the contents of the webpage
+
+st.text_input("")
