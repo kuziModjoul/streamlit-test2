7d6cf93ccba2d61087392a882a020276cc93abfa
kuziModjoul <kuzi@modjoul.com>
2022-11-15 12:23:59 -0500

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,12 +18,44 @@ with col2:
 with col3:
     st.write("")
 
-auth = False
-## We have  to authenticate the user before they can view the contents of the webpage
-if auth == False:
 
-    col1, col2, col3 = st.columns((.5,1,.5))
-    with col2:
-        st.markdown("### Please Authenticate using the Username and Password you were provided")
-        username = st.text_input("Username...")
-        password = st.text_input("Password...")
+
+
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if (
+            st.session_state["username"] in st.secrets["passwords"]
+            and st.session_state["password"]
+            == st.secrets["passwords"][st.session_state["username"]]
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store username + password
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show inputs for username + password.
+        st.text_input("Username", on_change=password_entered, key="username")
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input("Username", on_change=password_entered, key="username")
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 User not known or password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
+if check_password():
+    st.write("Here goes your normal Streamlit app...")
+    st.button("Click me")
